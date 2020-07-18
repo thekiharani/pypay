f702565e600c7dc1c3ffc0e3cee8e9f63c7cd9c5
Joe Gitonga <thekiharani@gmail.com>
2020-07-18 09:02:34 +0300

    custome LMO model-admin

--- daraja/admin.py
+++ daraja/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from daraja.models import ExpressPay
 
-admin.site.register(ExpressPay)
+class ExpressPayAdmin(admin.ModelAdmin):
+    list_display = ('PhoneNumber', 'Amount', 'MpesaReceiptNumber', 'TransactionDate')
+
+admin.site.register(ExpressPay, ExpressPayAdmin)
