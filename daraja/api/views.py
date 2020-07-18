7a83519f9265e8ce6f68c076420a63b878c06f16
Joe Gitonga <thekiharani@gmail.com>
2020-07-18 14:46:55 +0300

    debug the txn response

--- daraja/api/views.py
+++ daraja/api/views.py
@@ -16,30 +16,19 @@ class ExpressCallback(CreateAPIView):
     permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
-        loggers.log_pay('This worked...')
+        loggers.log_pay('Txn Start')
         loggers.log_pay(request.data)
         merchant_request_id = request.data['Body']['stkCallback']['MerchantRequestID']
-        loggers.log_pay({'MerchantRequestID': merchant_request_id})
         checkout_request_id = request.data['Body']['stkCallback']['CheckoutRequestID']
-        loggers.log_pay({'CheckoutRequestID': checkout_request_id})
         result_code = request.data['Body']['stkCallback']['ResultCode']
-        loggers.log_pay({'ResultCode': result_code})
         result_desc = request.data['Body']['stkCallback']['ResultDesc']
-        loggers.log_pay({'ResultDesc': result_desc})
         amount = request.data['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value']
-        loggers.log_pay({'Amount': amount})
         receipt_number = request.data['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value']
-        loggers.log_pay({'MpesaReceiptNumber': receipt_number})
         balance = ""
-        loggers.log_pay({'Balance': balance})
         txn_date = request.data['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value']
-        loggers.log_pay({'TransactionDate': txn_date})
         str_txn_date = str(txn_date)
-        loggers.log_pay({'TransactionDate': str_txn_date})
         txn_datetime = datetime.strptime(str_txn_date, "%Y%m%d%H%M%S")
-        loggers.log_pay({'TransactionDate': txn_datetime})
         phone_number = request.data['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value']
-        loggers.log_pay({'PhoneNumber': phone_number})
 
         txn = ExpressPay.objects.create(
             MerchantRequestID = merchant_request_id,
@@ -53,5 +42,6 @@ class ExpressCallback(CreateAPIView):
             PhoneNumber = phone_number
         )
         txn.save()
+        loggers.log_pay('Txn End\n')
         print(request.data)
         return Response(data={'message': 'success'}, status=status.HTTP_201_CREATED)
