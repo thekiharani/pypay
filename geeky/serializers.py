10a5ba73ef384ad1a22ec6163e06037844cdc408
Joe Gitonga <thekiharani@gmail.com>
2020-07-29 02:42:09 +0300

    added notes content

--- geeky/serializers.py
+++ geeky/serializers.py
@@ -5,5 +5,5 @@ from . models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ['id', 'title', 'completed', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'description', 'completed', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
