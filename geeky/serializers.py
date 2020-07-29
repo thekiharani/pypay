de95f7231bda2be998cf89a8c891e947d49b7973
Joe Gitonga <thekiharani@gmail.com>
2020-07-29 03:06:19 +0300

    limit content in listing

--- geeky/serializers.py
+++ geeky/serializers.py
@@ -7,3 +7,9 @@ class NoteSerializer(serializers.ModelSerializer):
         model = Note
         fields = ['id', 'title', 'description', 'completed', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
+
+class NoteListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'completed', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
--- geeky/views.py
+++ geeky/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets, permissions
 
 from geeky.models import Note
-from geeky.serializers import NoteSerializer
+from geeky.serializers import NoteSerializer, NoteListSerializer
 
 
 class NoteView(viewsets.ModelViewSet):
     permission_classes = (permissions.AllowAny,)
     queryset = Note.objects.all()
-    serializer_class = NoteSerializer
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return NoteListSerializer
+        return NoteSerializer
